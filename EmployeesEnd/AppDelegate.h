d556c762482f34700ac3be2e5af79a49793c7b4c
邓梦超 <dengMC@dengmengchaodemacbook-air.local>
2016-11-08 16:04:52 +0800

    Initial Commit

---
+++ EmployeesEnd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmployeesEnd
+//
+//  Created by 邓梦超 on 16/11/8.
+//  Copyright © 2016年 邓梦超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmployeesEnd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmployeesEnd
+//
+//  Created by 邓梦超 on 16/11/8.
+//  Copyright © 2016年 邓梦超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
